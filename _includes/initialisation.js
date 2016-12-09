b36d5dfb4abe500a00ec34eb39df7817b3144ff2
Thibault Coupin <thibault.coupin@ign.fr>
2016-12-09 22:58:51 +0100

    remove markdown

--- _includes/initialisation.js
+++ _includes/initialisation.js
@@ -3,8 +3,6 @@ Reveal.initialize({
 	history: true,
 	// More info https://github.com/hakimel/reveal.js#dependencies
 	dependencies: [
-		{ src: '//cdn.rawgit.com/hakimel/reveal.js/{{ site.reveal.version }}/plugin/markdown/marked.js' },
-		{ src: '//cdn.rawgit.com/hakimel/reveal.js/{{ site.reveal.version }}/plugin/markdown/markdown.js' },
 		{ src: '//cdn.rawgit.com/hakimel/reveal.js/{{ site.reveal.version }}/plugin/notes/notes.js', async: true },
 		{ src: '//cdn.rawgit.com/hakimel/reveal.js/{{ site.reveal.version }}/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
 	]
---
+++ _presentations/my_first_presentation.md
@@ -0,0 +1,32 @@
+---
+title: Ma première présentation
+subtitle: coucou
+---
+
+## Intro
+On utilise le markdown pour la mise en forme.
+On débute une nouvelle diapositive avec le mot clé "§ new" (sans espace)
+
+§new
+
+## Faire des notes
+Pour faire des notes, c'est "§ notes" (sans espace)
+
+S pour voir les notes
+
+§notes
+Ceci est une note
+
+§new
+
+## Diapo verticales
+
+Il suffit de remplacer le "§ new" par "§ ᠎break"  (sans espace)
+
+§break
+
+## Slide 3.1
+
+§break
+
+## Slide 3.2
